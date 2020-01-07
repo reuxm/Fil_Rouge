a6ac7782300423eaa884bca440e3308ceacc16c7
matthias <matthias@KaaK756U.lan>
2020-01-07 13:49:35 +0100

    Architecture de base

---
+++ FileRouge/src/main/java/Groupe1/FileRouge/App.java
@@ -0,0 +1,13 @@
+package Groupe1.FileRouge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
