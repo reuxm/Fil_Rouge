e8cd41ff67420c32353c34ea3e02c56b38dbcefe
matthias <matthias@KaaK756U.lan>
2020-01-16 16:05:22 +0100

    service client

---
+++ FileRouge/src/main/java/groupe1/filrouge/service/ServiceClient.java
@@ -0,0 +1,42 @@
+package groupe1.filrouge.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import groupe1.filrouge.dao.ClientDao;
+import groupe1.filrouge.entity.Client;
+
+@Service
+public class ServiceClient implements IServiceClient {
+
+	@Autowired
+	private ClientDao dao;
+	
+	@Override
+	public List<Client> list() {
+		return dao.findAll();
+	}
+
+	@Override
+	public Client find(Integer id) {
+		return dao.findById( id ).get();
+	}
+
+	@Override
+	public void create(Client c) {
+		dao.save( c );
+	}
+
+	@Override
+	public void update(Client c) {
+		dao.save( c );
+	}
+
+	@Override
+	public void delete(Client c) {
+		dao.deleteById( c.getId() );
+	}
+
+}
