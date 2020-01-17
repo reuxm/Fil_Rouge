b23787588797eeb91e0f8321d461dc8ca20e30c9
Gaied <34006535+Gaied@users.noreply.github.com>
2020-01-17 11:48:59 +0100

    JQUERY

---
+++ FileRouge/src/main/java/groupe1/filrouge/entity/Priorite.java
@@ -0,0 +1,37 @@
+package groupe1.filrouge.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "priorite") 
+public class Priorite {
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)		
+	private Integer id;
+	
+	@Column(name="libelle", length = 50, nullable = false)
+	private String libelle;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	
+}
