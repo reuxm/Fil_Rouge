a749d1199033c33c53baedacb0336edef59f44b2
KuZJr <ikaru1999@gmail.com>
2020-01-20 14:37:38 +0100

    CRUD Commande Piece

---
+++ FileRouge/src/main/java/groupe1/filrouge/controller/form/CommandePieceForm.java
@@ -0,0 +1,52 @@
+package groupe1.filrouge.controller.form;
+
+public class CommandePieceForm {
+	private Integer id;
+	private Integer id_user;
+	private Integer id_piece;
+	
+	private Integer qte;
+	
+	private String dateCreation;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId_user() {
+		return id_user;
+	}
+
+	public void setId_user(Integer id_user) {
+		this.id_user = id_user;
+	}
+
+	public Integer getId_piece() {
+		return id_piece;
+	}
+
+	public void setId_piece(Integer id_piece) {
+		this.id_piece = id_piece;
+	}
+
+	public Integer getQte() {
+		return qte;
+	}
+
+	public void setQte(Integer qte) {
+		this.qte = qte;
+	}
+
+	public String getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(String dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+}
