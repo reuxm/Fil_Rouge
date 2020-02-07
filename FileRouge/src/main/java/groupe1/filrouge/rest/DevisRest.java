3258d0e7d235bcc88df33af364f76d45df9018c3
Gaied <34006535+Gaied@users.noreply.github.com>
2020-02-07 14:24:02 +0100

    Update DevisRest.java
    Modification

---
+++ FileRouge/src/main/java/groupe1/filrouge/rest/DevisRest.java
@@ -0,0 +1,38 @@
+package groupe1.filrouge.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import groupe1.filrouge.entity.Devis;
+import groupe1.filrouge.service.IServiceDevis;
+
+@RestController
+@RequestMapping("/Rest/entity")
+public class DevisRest {
+
+	@Autowired
+	private IServiceDevis sdevis;
+
+	@GetMapping("/devis")
+	public List<Devis> listdevis() {
+		return sdevis.list();
+	}
+
+	@PostMapping("/createdevis")
+	public void createD(@RequestBody Devis d) {
+		sdevis.create(d);
+	}
+
+	@GetMapping("/devis/{id}")
+	public Devis find(@PathVariable("id") int id) {
+		return sdevis.find(id);
+	}
+
+}
