32730429f71352a1dfe0c5db9526d91b135edda6
Gaied <34006535+Gaied@users.noreply.github.com>
2020-02-07 13:57:34 +0100

    Update PiecesRest.java
    update

--- FileRouge/src/main/java/groupe1/filrouge/rest/PiecesRest.java
+++ FileRouge/src/main/java/groupe1/filrouge/rest/PiecesRest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import groupe1.filrouge.entity.Piece;
@@ -19,6 +20,11 @@ public class PiecesRest {
 	@GetMapping("/pieces")
 	public List<Piece> listpieces() {
 		return spiece.recherchePiece();
+	}
+	
+	@GetMapping("/pieces/{id}")
+	public Piece find( @PathVariable("id") int id ) {
+		return spiece.recherchePieceId(id);
 	}
 
 }
