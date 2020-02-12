4a3dec1ee38b893b24f08e457b0525ac55bba5da
KuZJr <ikaru1999@gmail.com>
2020-02-12 17:27:28 +0100

    Recherche de pièces

--- FileRouge/src/main/java/groupe1/filrouge/rest/PiecesRest.java
+++ FileRouge/src/main/java/groupe1/filrouge/rest/PiecesRest.java
@@ -3,6 +3,7 @@ package groupe1.filrouge.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import groupe1.filrouge.entity.Piece;
 import groupe1.filrouge.service.IServicePiece;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/Rest/entity")
 public class PiecesRest {
