a3cef8778ad557e6c0b1e288b1277e2db7540840
mbaumont <mariebaumont60@gmail.com>
2020-02-12 16:11:59 +0100

    Ajout de la liste de commandes véhicules
    Visualisation + modification avec le bouton livré

--- AppAngular/src/app/app-routing.module.ts
+++ AppAngular/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ListeVehiculesComponent } from './liste-vehicules/liste-vehicules.component'
+import { ListeVehiculesComponent } from './liste-vehicules/liste-vehicules.component';
 import { DevisComponent } from './devis/devis.component';
 import { ListTachesComponent } from './list-taches/list-taches.component';
 import { FichesComponent } from './fiches/fiches.component';
 import { TacheDetailComponent } from './tache-detail/tache-detail.component';
+import { ListeCommandeVehiculesComponent } from './liste-commande-vehicules/liste-commande-vehicules.component';
 
 
 
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'vehicule', component: ListeVehiculesComponent},
   {path: 'devis', component: DevisComponent},
   { path: 'fiches', component: FichesComponent },
+  { path: 'commandeVeh', component: ListeCommandeVehiculesComponent},
   {
     path: 'taches',
     component: ListTachesComponent,
