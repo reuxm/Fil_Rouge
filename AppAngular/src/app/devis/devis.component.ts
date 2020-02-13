9ee6dcd84d2d9133c0daa57178d9b1bb128ac8aa
mbaumont <mariebaumont60@gmail.com>
2020-02-13 14:32:30 +0100

    ModifMineurs

--- AppAngular/src/app/devis/devis.component.ts
+++ AppAngular/src/app/devis/devis.component.ts
@@ -1,59 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { Devis, Client } from 'src/app/_models/devis.model';
-import {DevisService} from '../_services/devis/devis.service';
+import { Component, OnInit } from "@angular/core";
+import { Devis, Client } from "src/app/_models/devis.model";
+import { DevisService } from "../_services/devis/devis.service";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
-import { Router, ActivatedRoute } from '@angular/router';
-import { identifierModuleUrl } from '@angular/compiler';
-import { Vehicule } from '../_models/devis.model';
-import { User } from '../_models/tache.model';
-import { forkJoin } from 'rxjs';
-import { Router } from '@angular/router';
-import { LoginService } from '../login/login.service';
+import { Router, ActivatedRoute } from "@angular/router";
+import { identifierModuleUrl } from "@angular/compiler";
+import { Vehicule } from "../_models/devis.model";
+import { User } from "../_models/tache.model";
+import { forkJoin } from "rxjs";
+import { Router } from "@angular/router";
+import { LoginService } from "../login/login.service";
 
 @Component({
-  selector: 'app-devis',
-  templateUrl: './devis.component.html',
-  styleUrls: ['./devis.component.css']
+  selector: "app-devis",
+  templateUrl: "./devis.component.html",
+  styleUrls: ["./devis.component.css"]
 })
 export class DevisComponent implements OnInit {
   addForm: FormGroup;
   submitted: boolean;
 
-
-
-  constructor(private sdevis: DevisService, private logServ: LoginService, private router: Router,
-    private formBuilder: FormBuilder) { }
+  constructor(
+    private sdevis: DevisService,
+    private logServ: LoginService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-	  if( !this.logServ.isCom )
-		  this.router.navigate(['\home']);
-    this.getAllDevis();
-
+    if (!this.logServ.isCom) {
+      this.router.navigate(["home"]);}
+    this.route.queryParamMap.subscribe( d => {
+      this.getAllDevis();
+    });
 
     this.addForm = this.formBuilder.group({
-      dateCreation:  ['', Validators.required],
+      dateCreation: ["", Validators.required],
       client: this.formBuilder.group({
-        name: '',
-        id: ''
+        name: "",
+        id: ""
       }),
       vehicule: this.formBuilder.group({
-        modele:'',
-        id: ''
+        modele: "",
+        id: ""
       }),
       user: this.formBuilder.group({
-        firstname: '',
-        id: ''
-      }),
-
+        firstname: "",
+        id: ""
+      })
     });
   }
 
   getAllDevis(): void {
-    this.sdevis.getAllDevis()
-    .subscribe(devis => {
+    this.sdevis.getAllDevis().subscribe(devis => {
       this.devis = devis;
-    } );
+    });
   }
 
   onSubmit() {
@@ -64,22 +66,23 @@ export class DevisComponent implements OnInit {
     const clientP = this.sdevis.getClient(parseInt(formValue.client.id));
     const userP = this.sdevis.getUser(parseInt(formValue.user.id));
     forkJoin([vehiculeP, clientP, userP]).subscribe(responses => {
-      const devis = new Devis(
-        {dateCreation: formValue.dateCreation,
-          client: responses[1],
-          vehicule: responses[0],
-          user: responses[2],
-          etat: false}
-      );
+      const devis = new Devis({
+        dateCreation: formValue.dateCreation,
+        client: responses[1],
+        vehicule: responses[0],
+        user: responses[2],
+        etat: false
+      });
       console.log(devis);
-      this.sdevis.createDevis(devis)
-      .subscribe( devis => {
-        this.router.navigate(['/devis'], {queryParams: {ok: true} } );
+      this.sdevis.createDevis(devis).subscribe(devis => {
+        this.router.navigate(["/devis"], { queryParams: { ok: true } });
         console.warn(devis);
       });
     });
   }
 
   // get the form short name to access the form fields
-  get f() { return this.addForm.controls; }
+  get f() {
+    return this.addForm.controls;
+  }
 }
